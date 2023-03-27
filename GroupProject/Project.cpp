8c5e76900ce83dff65d535fca3e81fbd846d754d
Nickeem <vacation1478@gmail.com>
2023-03-26 23:23:09 -0400

    Added Better Camera

--- GroupProject/Project.cpp
+++ GroupProject/Project.cpp
@@ -34,14 +34,34 @@ Install-Package Assimp -version 3.0.0
 //ANY INCLUDES GO HERE 
 #include "shader.h";
 #include "model.h";
+#include "camera.h";
 
 
 
 GLFWwindow* window;
 
 // properties
-GLuint screenWidth = 800, screenHeight = 800;
+GLuint screenWidth = 1000, screenHeight = 800;
 GLfloat scale_size = 1.f;
+
+//Initial location of camera
+glm::vec3 camLocation(0.0f, 0.0f, 1500.0f);
+
+// Camera
+Camera camera(camLocation);
+float lastX = screenWidth / 2.0f;
+float lastY = screenHeight / 2.0f;
+bool mouseMoved = true;
+
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
+void keyboardCallback(GLFWwindow* window, int key, int scancode, int action, int modes);
+//void mouseClickedCallback(GLFWwindow* window, int button, int  action, int mode);
+//void moveMouseCallback(GLFWwindow* window, double xpos, double ypos);
+void clickDragCallback(GLFWwindow* window, int button, int  action, int mode);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
 
 void init_Resources()
 {
@@ -82,6 +102,19 @@ void init_Resources()
         exit(EXIT_FAILURE);
     }
 
+    //----------------------------------------------------
+    // Registering the call-back function for the keyboard
+    //----------------------------------------------------
+    glfwSetKeyCallback(window, keyboardCallback);
+
+    //----------------------------------------------------
+        // Registering the call-back functions for the mouse
+        //----------------------------------------------------
+    // glfwSetMouseButtonCallback(window, mouseClickedCallback);
+    glfwSetMouseButtonCallback(window, clickDragCallback);
+    //glfwSetCursorPosCallback(window, moveMouseCallback);
+    glfwSetScrollCallback(window, scroll_callback);
+
     // Setup OpenGL options
     glEnable(GL_DEPTH_TEST);
 
@@ -106,39 +139,42 @@ int main()
 
     GLuint viewID = glGetUniformLocation(objshader.Program, "view");
 
-    // =======================================================================
-    //  Set the projection matrix
-    // =======================================================================
-    glm::mat4 projection = glm::perspective(45.0f, // Field of Vision
-        (GLfloat)screenWidth / (GLfloat)screenHeight, // Aspect Ratio
-        1.0f,
-        10000.0f // camera position
-    ); 
+    
 
-    objshader.Use();
-    glUniformMatrix4fv(glGetUniformLocation(objshader.Program, "projection"),
-        1, GL_FALSE, glm::value_ptr(projection));
+    
 
 
 
     //Keep displaying the window until we have shut it down
     while (!glfwWindowShouldClose(window))
     {
+        // per-frame time logic
+        // --------------------
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
 
         // Clear buffers
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        
+        // =======================================================================
+    //  Set the projection matrix
+    // =======================================================================
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), // Field of Vision
+            (GLfloat)screenWidth / (GLfloat)screenHeight, // Aspect Ratio
+            1.0f,
+            10000.0f // camera position
+        );
+
+        objshader.Use();
+        glUniformMatrix4fv(glGetUniformLocation(objshader.Program, "projection"),
+            1, GL_FALSE, glm::value_ptr(projection));
+
         // =======================================================================
         // Step 4. create the View matrix
         // =======================================================================
-        View = glm::lookAt
-        (
-            glm::vec3(500, 500, 1300), // Camera position in World Space
-            glm::vec3(0, 0, 0),  // and looking at the origin
-            glm::vec3(0, 1, 0)   // Head is up (set to 0,-1,0 to look upside-down)
-        );
+        View = camera.GetViewMatrix();
 
         objshader.Use();
         glUniformMatrix4fv(viewID, 1,
@@ -179,4 +215,80 @@ int main()
     //Release ALL the other resources
     glfwTerminate();
     exit(EXIT_SUCCESS);
+}
+
+
+
+
+void keyboardCallback(GLFWwindow* window, int key, int scancode, int action, int modes)
+{
+    // If ESC is pressed, close the window
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+        return;
+    }
+
+
+    // move camera
+    const float cameraSpeed = 100.f; // adjust accordingly
+    if (key == GLFW_KEY_UP && action == GLFW_PRESS) {
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    }
+    if (key == GLFW_KEY_DOWN && action == GLFW_PRESS) {
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    }
+    if (key == GLFW_KEY_LEFT && action == GLFW_PRESS) {
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    }
+    if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS) {
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+    }
+
+}
+
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
+
+
+void clickDragCallback(GLFWwindow* window, int button, int  action, int mode)
+{
+    static double startX = 0, startY = 0, endX = 0, endY = 0;
+
+    while (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    {
+        // Make sure that the button is held down
+        glfwSetInputMode(window, GLFW_STICKY_MOUSE_BUTTONS, GL_TRUE);
+
+
+        // ------Do things here -------
+        //e.g. Catch starting XY location of the mouse pointer
+        glfwGetCursorPos(window, &startX, &startY);
+
+        cout << "\n\nBegin Dragging Mouse... ";
+        if (action != GLFW_RELEASE) {
+            break;
+        }
+
+    }
+
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
+    {
+        cout << "\nEnd Draging Mouse...\n";
+
+        // ------Do other things here -------
+        //e.g. Catch ending XY location of the mouse pointer
+        glfwGetCursorPos(window, &endX, &endY);
+        GLfloat xoffset = startX - lastX;
+        GLfloat yoffset = lastY - startY;
+        camera.ProcessMouseMovement(xoffset, yoffset);
+
+        cout << "\nMouse has moved from X : " << startX << " to " << endX;
+        cout << "\nMouse has moved from Y : " << startY << " to " << endY;
+    }
 }
